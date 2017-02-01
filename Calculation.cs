203b979547e1e77a26adff27009d8c1329121794
axelmark <axelmark@outlook.com>
2017-02-01 18:29:47 +0700

    1

---
+++ Calculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    delegate double CalcDeligate(double a, double b);
+
+    class Calculation
+    {
+        public double calc(double value1, double value2, string sing)
+        {
+
+            return value1 + value2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calc = new Calculation();
+            Console.OutputEncoding = Encoding.UTF7;
+            Console.WriteLine("Введите первое значение: ");
+            string value1 = Console.ReadLine();
+            Console.WriteLine("Введите знак (+-*/): ");
+             string sing = Console.ReadLine();
+            Console.WriteLine("Введите второе значение: ");
+            string value2 = Console.ReadLine();
+ 
+
+            double res = calc.calc(Convert.ToDouble(value1), Convert.ToDouble(value2), sing);
+            Console.WriteLine($"({value1} {sing} {value2} = {res})");
+            Console.ReadLine();
+        }
+    }
+}
